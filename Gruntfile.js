207677d252cf21e518a3b59c14bd140e27f0137b
Arthur Darcet <arthur.darcet@m4x.org>
2013-04-05 10:02:50 +0200

    Update to grunt 0.4

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+						'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+						'* <%= pkg.url %>\n' +
+						'* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+						'<%= pkg.name %> */\n'
+			},
+			reaaad: {
+				files: {
+					'reaaad.js': [
+						"js-src/jquery.min.js",
+						"js-src/mustache.js",
+						"js-src/moment.min.js",
+						"js-src/jquery.cookie.js",
+						"js-src/monocle.overlay.js",
+						"js-src/reader.js",
+					]
+				}
+			}
+		},
+		watch: {
+			reaaad: {
+				files: [
+					"js-src/jquery.min.js",
+					"js-src/mustache.js",
+					"js-src/moment.min.js",
+					"js-src/jquery.cookie.js",
+					"js-src/monocle.overlay.js",
+					"js-src/reader.js",
+				],
+				task: ['uglify:reaaad']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', 'uglify');
+};
+
